fea9c35ce8b74e31b3338db12a0fa84412aab96e
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-05-03 15:16:03 -0700

    remove install.sh for jitx-client

--- .github/jitx-client/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-chmod +x /root/.jitx/current/jitx # artifact caching loses permissions somewhere
-cp /root/.jitx/current/jitx.config /root/.jitx # missing config file
-chmod +x /root/.jitx/current/jstanza
-chmod +x /root/.jitx/current/stanza/stanza
-apt install -y python3.8
