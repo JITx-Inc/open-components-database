a7d315944852a413fbb97ff3b3a0f7453588af75
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-01 18:42:38 -0700

    fix duplciation of cubemx-importer package name, isolate pure stanza code in cubemx-ipmorter-utils for test

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Launching ocdb tests depending on jitx-client..."
-cd open-components-database/tests/
-jitx run-test test-stm-pin-parsing.stanza
-cd ../..
-
 echo "Searching for pcb objects and generating tests..."
 python3.8 scripts/evaluate_pcb_objects.py
 cd test-evaluate/
