9a471600a99a917a504886d808f0f7b347f328bd
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-01 18:44:47 -0700

    put back ocdb unit-tests in docker contianer as we need stanza installed

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Launching ocdb unit-tests, they do not depend on jitx-client..."
+cd open-components-database
+stanza run-test tests/test-stm-pin-parsing.stanza
+cd ..
+
 echo "Searching for pcb objects and generating tests..."
 python3.8 scripts/evaluate_pcb_objects.py
 cd test-evaluate/
