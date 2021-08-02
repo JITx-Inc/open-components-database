744b59c600c1e19d1ab5800805a2f32ea7ed7e04
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-01 18:03:08 -0700

    fix and hook test

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Launching ocdb tests depending on jitx-client..."
+jitx run-test open-components-database/tests/test-stm-pin-parsing.stanza
+
 echo "Searching for pcb objects and generating tests..."
 python3.8 scripts/evaluate_pcb_objects.py
 cd test-evaluate/
