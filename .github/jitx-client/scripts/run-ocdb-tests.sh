86b8f011d8977b0628ff65477ba71868d01af308
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-09 20:30:22 -0700

    sort out basic integration tests

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Launching ocdb tests, they can depend on jitx-client..."
 cd open-components-database
-jitx run-test tests/test-stm-pin-parsing.stanza
+jitx run-test tests/test-ocdb.stanza
 cd ..
 
 echo "Searching for pcb objects and generating tests..."
