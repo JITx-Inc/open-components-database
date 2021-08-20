a873796179e0d5f2500176825b39c2dff33038a0
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-20 15:51:12 -0700

    Run ocdb designs in ocdb CI

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -6,6 +6,22 @@ cd open-components-database
 jitx run-test tests/test-stm-pin-parsing.stanza
 cd ..
 
+echo "Launching ocdb designs..."
+cd open-components-database/designs
+# Explicitly exclude tutorial.stanza and mcu.stanza because they query octopart more
+designs=(ble-mote.stanza
+         class-a.stanza
+         ethernet-fmc.stanza
+         smd-landpatterns.stanza
+         test-component-checks.stanza
+         voltage-divider.stanza)
+
+for filename in "${designs[@]}"; do
+    echo "Running $filename..."
+    jitx run $filename
+done
+cd ../..
+
 echo "Searching for pcb objects and generating tests..."
 python3.8 scripts/evaluate_pcb_objects.py
 cd test-evaluate/
