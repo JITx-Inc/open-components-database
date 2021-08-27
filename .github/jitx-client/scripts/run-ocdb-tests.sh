68299b14081d4c5d9ecc88580281dbbe4910cd0c
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-26 22:37:59 -0700

    move macro evaluation before tests

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Searching for pcb objects and generating tests..."
+python3.8 scripts/evaluate_pcb_objects.py
+cd test-evaluate/
+echo "Launching pcb object tests..."
+jitx run-test test/evaluate/api
+
 echo "Launching ocdb tests, they can depend on jitx-client..."
 cd open-components-database
 jitx run-test tests/test-stm-pin-parsing.stanza
@@ -22,8 +28,3 @@ for filename in "${designs[@]}"; do
 done
 cd ../..
 
-echo "Searching for pcb objects and generating tests..."
-python3.8 scripts/evaluate_pcb_objects.py
-cd test-evaluate/
-echo "Launching pcb object tests..."
-jitx run-test test/evaluate/api
