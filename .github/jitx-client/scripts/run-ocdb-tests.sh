c2684b6a50282b9c8a1a113d8873a931df189635
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-27 10:48:34 -0700

    fix CI

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -10,6 +10,7 @@ python3.8 scripts/evaluate_pcb_objects.py
 cd test-evaluate/
 echo "Launching pcb object tests..."
 jitx run-test test/evaluate/api
+cd ..
 
 echo "Launching ocdb tests, they can depend on jitx-client..."
 cd open-components-database
