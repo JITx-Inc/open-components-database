7ed8630fd952e510dd4b05af8d81b6fa7c511688
Philippe Ferreira De Sousa <philippe.fdesousa@gmail.com>
2021-08-30 13:02:41 -0700

    integration tests were using jitx-client docker pkgs instead of the source fiels form ocdb repo

--- .github/jitx-client/scripts/run-ocdb-tests.sh
+++ .github/jitx-client/scripts/run-ocdb-tests.sh
@@ -9,12 +9,24 @@ jitx check-install
 echo "/root/.jitx/user.params:"
 cat /root/.jitx/user.params
 
+#==============================================
+#==== public pcb-* macro evaluation tests =====
+#==============================================
+
+# Those macro evaluation tests take into account the root stanza.proj as it is included in the generated test-evaluate/stanza.proj
 echo "Searching for pcb objects and generating tests..."
 python3.8 scripts/evaluate_pcb_objects.py
 cd test-evaluate/
 echo "Launching pcb object tests..."
 jitx run-test test/evaluate/api
 cd ..
+
+#==============================================
+#====== Unit-tests and integration-tests ======
+#==============================================
+
+# Those tests need to take into account the root stanza.proj so that they find the source files from the ocdb repo and not a pkg from the jitx-client docker image
+export STANZA_CONFIG=/app/open-components-database/
 
 echo "Launching ocdb tests, they can depend on jitx-client..."
 cd open-components-database
