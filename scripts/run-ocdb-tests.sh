19458d70a9bd037d53aa793d1462daafe93c5649
Jason Watson <j.watson@jitx.com>
2023-08-08 13:52:51 -0400

    Move .github/jitx-client/scripts/* to scripts/* to improve usage of the test script by other repos.  Fixup Dockerfile to match.

--- scripts/run-ocdb-tests.sh
+++ scripts/run-ocdb-tests.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+PYTHON=python3.10
+# Tests need to take into account the root stanza.proj so that they find the source files from the ocdb repo and not a pkg from the jitx-client docker image
+JITX_RUN="${JITX} run /app/open-components-database/stanza.proj"
+JITX_RUN_TEST="${JITX} run-test /app/open-components-database/stanza.proj"
+
 # check for any optional argument to run fewer tests (skip Octopart queries, for example)
 if [[ $# -ne 0 ]]; then
     restrict=true
@@ -36,43 +41,48 @@ else
   echo "${JITX_HOME}/user.params does not exist"
 fi
 
-# Tests need to take into account the root stanza.proj so that they find the source files from the ocdb repo and not a pkg from the jitx-client docker image
-export JITX_RUN="${JITX} run /app/open-components-database/stanza.proj"
-export JITX_RUN_TEST="${JITX} run-test /app/open-components-database/stanza.proj"
-
 #==============================================
 #=========== Component Models test ============
 #==============================================
+(
 echo "Checking that ocdb/components files conform to spec and compile."
 cd open-components-database
 scripts/gen-components-file.sh
 jitx run components.stanza
+rm components.stanza
 cd ..
+)
 
 #==============================================
 #==== public pcb-* macro evaluation tests =====
 #==============================================
-
+(
 echo "Searching for pcb objects and generating tests..."
-python3.10 scripts/evaluate_pcb_objects.py
+cd open-components-database
+${PYTHON} scripts/evaluate_pcb_objects.py
 cd test-evaluate/
 echo "Launching pcb object tests..."
-$JITX_RUN_TEST test/evaluate/api
+${JITX_RUN_TEST} test/evaluate/api
 cd ..
+rm -rf test-evaluate
+cd ..
+)
 
 #==============================================
 #================= Unit-tests =================
 #==============================================
 
+(
 echo "Launching ocdb tests, they can depend on jitx-client..."
 cd open-components-database
-$JITX_RUN_TEST tests/all.stanza -not-tagged part-query long not-implemented-yet
+${JITX_RUN_TEST} tests/all.stanza -not-tagged part-query long not-implemented-yet
 cd ..
+)
 
 #==============================================
 #============= Integration tests ==============
 #==============================================
-
+(
 echo "Launching ocdb designs..."
 cd open-components-database/designs
 designs=(ble-mote.stanza
@@ -99,8 +109,8 @@ fi
 
 for filename in "${designs[@]}"; do
     echo "Running $filename..."
-    $JITX_RUN $filename
+    ${JITX_RUN} $filename
 done
 
 cd ../..
-
+)
